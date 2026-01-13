a9a6b1af434e8e42a7b05cf55f2ba33d78699d15
gurleenv15 <gurleenv15@gmail.com>
2026-01-13 00:02:21 -0600

    Update 01_setup_and_data_load.R

--- teenage-driving-rd-analysis/code/01_setup_and_data_load.R
+++ teenage-driving-rd-analysis/code/01_setup_and_data_load.R
@@ -1,1 +1,15 @@
+# Purpose:
+# Load mortality data and construct death rates per 100,000 person-years
 
+library(tidyverse)
+library(haven)
+library(ggplot2)
+library(broom)
+
+mort <- read_dta("https://julianreif.com/driving/data/mortality/derived/all.dta")
+
+mort <- mort %>%
+  mutate(
+    cod_any_rate = 100000 * cod_any / (pop / 12),
+    cod_mva_rate = 100000 * cod_MVA / (pop / 12)
+  ) 
